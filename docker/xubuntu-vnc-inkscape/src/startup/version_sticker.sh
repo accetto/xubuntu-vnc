f2c7804ac2f00d4d2479931ef0fc20abddeb5583
accetto <34798830+accetto@users.noreply.github.com>
2019-09-08 20:28:05 +0200

    inkscape: release 19.09.1

---
+++ docker/xubuntu-vnc-inkscape/src/startup/version_sticker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+### @accetto, September 2019
+
+ubuntu=$("${STARTUPDIR}/version_of.sh" ubuntu)
+inkscape=$("${STARTUPDIR}/version_of.sh" inkscape)
+firefox=$("${STARTUPDIR}/version_of.sh" firefox)
+
+case "$1" in
+    -v)
+        echo "Ubuntu $ubuntu"
+        echo "Inkscape $inkscape"
+        [[ -n "$firefox" ]] && echo "Firefox $firefox"
+        ;;
+    -V)
+        mousepad=$("${STARTUPDIR}/version_of.sh" mousepad)
+        vim=$("${STARTUPDIR}/version_of.sh" vim)
+        echo "Ubuntu $ubuntu"
+        echo "Mousepad $mousepad"
+        echo "VIM $vim"
+        echo "Inkscape $inkscape"
+        [[ -n "$firefox" ]] && echo "Firefox $firefox"
+        ;;
+    *)
+        ### example: ubuntu18.04.3-inkscape0.92.3
+        sticker="ubuntu$ubuntu"-"inkscape$inkscape"
+        if [[ -n "$firefox" ]] ; then
+            ### example: ubuntu18.04.3-inkscape0.92.3-firefox69.0
+            sticker="$sticker"-"firefox$firefox"
+        fi
+        echo "$sticker"
+        ;;
+esac
