9fab2fd62e587379d6aa980018257283d90f5b0a
accetto <34798830+accetto@users.noreply.github.com>
2019-10-21 09:23:27 +0200

    base: added curl, git, wget, jq

--- docker/xubuntu-vnc/src/startup/version_sticker.sh
+++ docker/xubuntu-vnc/src/startup/version_sticker.sh
@@ -11,10 +11,16 @@ case "$1" in
         mousepad=$("${STARTUPDIR}/version_of.sh" mousepad)
         vim=$("${STARTUPDIR}/version_of.sh" vim)
         tigervnc=$("${STARTUPDIR}/version_of.sh" tigervnc)
+        curl=$("${STARTUPDIR}/version_of.sh" curl)
+        git=$("${STARTUPDIR}/version_of.sh" git)
+        jq=$("${STARTUPDIR}/version_of.sh" jq)
         echo "Ubuntu $ubuntu"
         echo "Mousepad $mousepad"
         echo "VIM $vim"
         echo "TigerVNC $tigervnc"
+        echo "curl $curl"
+        echo "Git $git"
+        echo "jq $jq"
         ;;
     *)
         ### example: ubuntu18.04.3
